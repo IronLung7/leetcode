d42c164775bc82f73d0edeb369ce55bb1032fc28
nono <357930204@qq.com>
2017-03-21 13:04:21 +0800

    3.21 #64 minimum path sum & #504 base7

---
+++ src/main/nono/_504_base7/Solution.java
@@ -0,0 +1,25 @@
+package _504_base7;
+
+/**
+ * Created by apple on 2017/2/28.
+ */
+public class Solution {
+    public String convertToBase7(int num) {
+        if (num == 0)
+            return "0";
+        boolean flag = (num > 0) ? true : false;
+        num = (num > 0) ? num : num * (-1);
+        String result = "";
+        int tmp;
+        while (num != 0) {
+            tmp = num % 7;
+            num /= 7;
+            result = String.valueOf(tmp) + result;
+        }
+        if (!flag) {
+            result = "-" + result;
+        }
+        return result;
+
+    }
+}
